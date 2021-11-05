dee7031a16b419ea469e421749bc58b76ad2c469
8picoz <8picoz@tata.pw>
2021-11-05 17:12:35 +0900

    [feat] file_path受け取り

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matched = App::new("mitsuba-raster")
+        .version("0.1")
+        .author("tata <8picoz@tata.pw>")
+        .about("rasterizer")
+        .arg(Arg::with_name("file_path")
+            .short("f")
+            .long("file_path")
+            .value_name("file_path")
+            .takes_value(true)
+            .required(true)
+        )
+        .get_matches();
+
+    let file_path = matched.value_of("file_path").unwrap();
+
+    
 }
